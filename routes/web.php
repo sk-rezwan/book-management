fb009c14216a6e9bb1c635ff8ed5b963246822da
Zahid Hasan <mdjahidhasan920@gmail.com>
2021-01-13 19:50:22 +0600

    unnecessary view deleted

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,9 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Support\Facades\Route;
 
 Route::get('/test', function(){
   return view('test');
 });
-Route::post('/issueBookSubmit', 'BooksIssuedController@index');
 
 Route::get('/booksList', 'BooksController@index');
 Route::get('/addBooks', 'BooksController@create');
@@ -24,25 +13,6 @@ Route::get('/categoriesList','BooksCategoryController@index');
 Route::get('/addCategories', 'BooksCategoryController@create');
 Route::post('/addCategories', 'BooksCategoryController@store');
 
-
-Route::get('/issuedBooks','BooksIssuedController@index');
-Route::post('/issueBooks', 'BooksIssuedController@store');
-Route::get('/issueBooks', 'BooksIssuedController@create');
-
-
-Route::get('/membersList', 'MemberController@index');
-Route::get('/addMembers',  'MemberController@create');
-Route::post('/addMembers', 'MemberController@store');
-
-Route::get('/returnBooks', 'BooksReturnedController@create');
-Route::post('/returnBooks', 'BooksReturnedController@store');
-
-Route::get('/addThesis', 'ThesisController@create');
-Route::post('/addThesis', 'ThesisController@store');
-
-Route::get('/thesisList', function () {
-    return view('thesisList');
-});
 
 Route::get('/', function () {
     return view('welcome');
